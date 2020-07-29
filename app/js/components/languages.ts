34070fd5d4fe6d24f58ceaca2a48e27bcd47e624
Amyas Merivale <merivale@gmail.com>
2020-07-29 09:20:54 +0200

    fixed language view bug

--- app/js/components/languages.ts
+++ app/js/components/languages.ts
@@ -5,30 +5,25 @@ import { Language } from '../state/languages'
 import state from '../state/index'
 import highlight from '../compiler/highlight'
 
-export default init()
+// get relevant elements
+const codeElements = document.querySelectorAll('code[data-language]') as NodeListOf<HTMLElement>
 
-/** initialise language features */
-function init (): void {
-  // get relevant elements
-  const codeElements = document.querySelectorAll('code[data-language]') as NodeListOf<HTMLElement>
-
-  // add syntax highlighting to code elements
-  for (const code of codeElements) {
-    code.innerHTML = highlight(code.innerText, code.dataset.language as Language)
-  }
-
-  // register to keep in sync with system state
-  state.on('languageChanged', language)
+// add syntax highlighting to code elements
+for (const code of codeElements) {
+  code.innerHTML = highlight(code.innerText, code.dataset.language as Language)
 }
 
+// register to keep in sync with system state
+state.on('languageChanged', language)
+
 /** updates the page to reflect language change */
-function language (language: Language): void {
+function language (): void {
   // get relevant elements
   const languageElements = document.querySelectorAll('[data-language]') as NodeListOf<HTMLElement>
 
   for (const element of languageElements) {
     // show/hide elements according to language
-    if (language === element.dataset.language || element.id === 'turtle') {
+    if (state.language === element.dataset.language || element.id === 'turtle') {
       element.classList.remove('hidden')
     } else {
       element.classList.add('hidden')
